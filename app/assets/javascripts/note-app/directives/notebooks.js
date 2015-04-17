6b359adb132192481ac501f7f20180c16fa76a54
Captain Mike <mike@captain-refactor.co.za>
2015-04-17 09:09:58 +0200

    highlight notebook navigation

--- app/assets/javascripts/note-app/directives/notebooks.js
+++ app/assets/javascripts/note-app/directives/notebooks.js
@@ -1,12 +1,15 @@
 angular.module('noteApp')
   .directive('notebooks', function() {
     return {
-      scope: {},
+      scope: {
+        notebookKey: '='
+      },
       templateUrl: "/javascripts/note-app/templates/notebooks.html",
       controller: 'NotebooksCtrl',
       controllerAs: 'NotebooksCtrl'
     };
   })
-  .controller('NotebooksCtrl', ['Notebook', function(Notebook) {
+  .controller('NotebooksCtrl', ['$scope', 'Notebook', function($scope, Notebook) {
+    this.notebookKey = $scope.notebookKey;
     this.notebooks = Notebook.all();
   }]);
