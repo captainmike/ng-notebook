ce64dd03d361882baf5f16f7bf46c02a5c88946b
Captain Mike <mike@captain-refactor.co.za>
2015-04-17 15:11:29 +0200

    add home component

--- app/assets/javascripts/note-app.js
+++ app/assets/javascripts/note-app.js
@@ -4,8 +4,9 @@ angular.module('noteApp', ['ngNewRouter', 'noteApp.components', 'noteApp.configs
   .controller('AppController', ['$router', AppController]);
 
 AppController.$routeConfig = [
-  {path: '/', component: 'notebook'},
-  {path: '/:key', component: 'notebook'}
+  {path: '/', component: 'home'},
+  {path: '/notebook', component: 'notebook'},
+  {path: '/notebook/:key', component: 'notebook'}
 ];
 
 function AppController($router) {}
---
+++ public/components/home/home.js
@@ -0,0 +1,3 @@
+angular.module('noteApp.components', [])
+  .controller('HomeController', function() {
+  });
